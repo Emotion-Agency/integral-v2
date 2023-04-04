5ac5158d9acf980a5feb32345f9e57ee654b2991
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-04-04 09:46:01 +0300

    feature: home-2 with adaptive complete

--- pages/index.vue
+++ pages/index.vue
@@ -23,17 +23,24 @@ definePageMeta({
       <AboutInfo class="home-2__about-text"
         >Empowering brands at the forefront of digital
       </AboutInfo>
-      <p class="home-2__text">
-        Integral is a multi-disciplinary design studio, catalysing insights with
-        technology to build thoughtful
-        <span class="home-2__underline-text">design-led solutions</span> for
-        dynamic global clients. <br /><br />
-        From strategy, to creative direction and production, we deliver
-        innovative and engaging experiences that
-        <span class="home-2__underline-text"
-          >connect brands with their audiences.</span
-        >
-      </p>
+      <div class="home-2__text-wrapper">
+        <p class="home-2__text home-2__text--active">
+          Integral is a multi-disciplinary design studio, catalysing insights
+          with technology to build thoughtful
+          <span class="home-2__underline-text">design-led solutions</span> for
+          dynamic global clients.
+        </p>
+        <p class="home-2__text">
+          Spanning strategy, creative direction and production, we deliver
+          innovative and engaging experiences that
+          <span class="home-2__underline-text"
+            >connect brands with their audiences.</span
+          >
+        </p>
+      </div>
+      <div class="home-2__pages-wrapper">
+        <p class="home-2__numbers"><span class="home-2__number">1</span> / 2</p>
+      </div>
     </div>
   </section>
 </template>
