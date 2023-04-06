6ce72e89870db0ec160eca21efe5fbbfd69bd572
letmewin22 <lkosteckiy5@gmail.com>
2023-04-06 12:03:08 +0300

    chore: some updates

--- pages/index.vue
+++ pages/index.vue
@@ -7,70 +7,55 @@ definePageMeta({
 </script>
 
 <template>
-  <section class="section section--nm home-1">
-    <video class="home-1__bg" autoplay muted loop>
-      <source src="/images/main.webm" type="video/webm" />
-    </video>
-    <div class="container home-1__wrapper">
-      <h1 class="home-1__title">
-        A multidisciplinary creative consultancy shaping
-        <span class="home-1__underline-text"> digital culture </span>
-      </h1>
-    </div>
-  </section>
-  <section class="section section--nm home-2">
-    <div class="container home-2__wrapper">
-      <AboutInfo class="home-2__about-text"
-        >Empowering brands at the forefront of digital
-      </AboutInfo>
-      <div class="home-2__text-wrapper">
-        <p class="home-2__text home-2__text--active">
-          Integral is a multi-disciplinary design studio, catalysing insights
-          with technology to build thoughtful
-          <span class="home-2__underline-text">design-led solutions</span> for
-          dynamic global clients.
-        </p>
-        <p class="home-2__text">
-          Spanning strategy, creative direction and production, we deliver
-          innovative and engaging experiences that
-          <span class="home-2__underline-text"
-            >connect brands with their audiences.</span
-          >
-        </p>
+  <main>
+    <section class="section section--nm home-1">
+      <video class="home-1__bg" autoplay muted loop>
+        <source src="/images/main.webm" type="video/webm" />
+      </video>
+      <div class="container home-1__wrapper">
+        <h1 class="home-1__title">
+          A multidisciplinary creative consultancy shaping
+          <span class="home-1__underline-text"> digital culture </span>
+        </h1>
       </div>
-      <div class="home-2__pages-wrapper">
-        <p class="home-2__numbers"><span class="home-2__number">1</span> / 2</p>
+    </section>
+    <section class="section section--nm home-2">
+      <div class="container home-2__wrapper">
+        <AboutInfo class="home-2__about-text"
+          >Empowering brands at the forefront of digital
+        </AboutInfo>
+        <div class="home-2__text-wrapper">
+          <p class="home-2__text home-2__text--active">
+            Integral is a multi-disciplinary design studio, catalysing insights
+            with technology to build thoughtful
+            <span class="home-2__underline-text">design-led solutions</span> for
+            dynamic global clients.
+          </p>
+          <p class="home-2__text">
+            Spanning strategy, creative direction and production, we deliver
+            innovative and engaging experiences that
+            <span class="home-2__underline-text"
+              >connect brands with their audiences.</span
+            >
+          </p>
+        </div>
+        <div class="home-2__pages-wrapper">
+          <p class="home-2__numbers">
+            <span class="home-2__number">1</span> / 2
+          </p>
+        </div>
       </div>
-    </div>
-  </section>
-  <section class="section section--nm home-3">
-    <div class="container home-3__wrapper">
-      <img class="home-3__content" src="/images/3.jpg" alt="Content" />
-    </div>
-  </section>
-  <section class="section section--nm home-4">
-    <div class="container home-4__wrapper">
-      <AboutInfo class="home-4__mob-text">About </AboutInfo>
+    </section>
+    <section class="section section--nm home-3">
+      <div class="container home-3__wrapper">
+        <img class="home-3__content" src="/images/3.jpg" alt="Content" />
+      </div>
+    </section>
+    <section class="section section--nm home-4">
+      <div class="container home-4__wrapper">
+        <AboutInfo class="home-4__mob-text">About </AboutInfo>
 
-      <div class="home-4__info-wrapper home-4__info-wrapper--1">
-        <p class="home-4__info-text">
-          Redefining
-          <span class="home-4__arrow-part">
-            <span class="home-4__arrow-text">&lt;&gt;</span> brand
-            <span class="home-4__arrow-text">&lt;/&gt;</span>
-          </span>
-          <span class="home-4__text-part">dimensionalities</span>
-          <IconsLeftAngleArrow class="home-4__arrow" />
-        </p>
-      </div>
-      <div class="home-4__img-wrapper">
-        <video class="home-4__img" autoplay muted loop>
-          <source src="/images/1.webm" type="video/webm" />
-        </video>
-      </div>
-      <div class="home-4__content">
-        <AboutInfo class="home-4__about-text">About </AboutInfo>
-        <div class="home-4__info-wrapper home-4__info-wrapper--2">
+        <div class="home-4__info-wrapper home-4__info-wrapper--1">
           <p class="home-4__info-text">
             Redefining
             <span class="home-4__arrow-part">
@@ -81,24 +66,43 @@ definePageMeta({
             <IconsLeftAngleArrow class="home-4__arrow" />
           </p>
         </div>
-        <div class="home-4__text-wrapper">
-          <p class="home-4__med-desc">
-            Operating at the intersection of physical & digital, specialising
-            in; <span class="home-4__underline-text">brand</span>,
-            <span class="home-4__underline-text">fashion</span>,
-            <span class="home-4__underline-text">product</span> &
-            <span class="home-4__underline-text">web3</span>.
-          </p>
-          <p class="home-4__small-desc">
-            With deep knowledge broadly spanning design, fashion & web3, we help
-            our partners grow through our innate understanding of the drivers
-            behind tomorrow’s trends and opportunities.
-          </p>
-          <div class="home-4__btn-wrapper">
-            <TheButton> Studio </TheButton>
+        <div class="home-4__img-wrapper">
+          <video class="home-4__img" autoplay muted loop>
+            <source src="/images/1.webm" type="video/webm" />
+          </video>
+        </div>
+        <div class="home-4__content">
+          <AboutInfo class="home-4__about-text">About </AboutInfo>
+          <div class="home-4__info-wrapper home-4__info-wrapper--2">
+            <p class="home-4__info-text">
+              Redefining
+              <span class="home-4__arrow-part">
+                <span class="home-4__arrow-text">&lt;&gt;</span> brand
+                <span class="home-4__arrow-text">&lt;/&gt;</span>
+              </span>
+              <span class="home-4__text-part">dimensionalities</span>
+              <IconsLeftAngleArrow class="home-4__arrow" />
+            </p>
+          </div>
+          <div class="home-4__text-wrapper">
+            <p class="home-4__med-desc">
+              Operating at the intersection of physical & digital, specialising
+              in; <span class="home-4__underline-text">brand</span>,
+              <span class="home-4__underline-text">fashion</span>,
+              <span class="home-4__underline-text">product</span> &
+              <span class="home-4__underline-text">web3</span>.
+            </p>
+            <p class="home-4__small-desc">
+              With deep knowledge broadly spanning design, fashion & web3, we
+              help our partners grow through our innate understanding of the
+              drivers behind tomorrow’s trends and opportunities.
+            </p>
+            <div class="home-4__btn-wrapper">
+              <TheButton> Studio </TheButton>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </main>
 </template>
